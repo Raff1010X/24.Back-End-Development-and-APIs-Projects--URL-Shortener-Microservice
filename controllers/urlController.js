4c8ef0098c5042938f28fa0125baab5196a7896f
Rafał Kowalczyk <kowalczykr@trend-glass.local>
2022-05-11 15:27:10 +0200

    working example

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 const validUrl = require('valid-url');
 
 const writeFile = async (url, req, res) => {
-    await fs.writeFile(`${__dirname}/../data/1`, url, (err) => {
+    const count = fs.readdirSync(`${__dirname}/../data`).length + 1;
+    fs.writeFile(`${__dirname}/../data/${count}`, url, (err) => {
         if (err) {
             res.json({ error: err });
         } else {
-            res.json({ original_url: url, short_url: 1 });
+            res.json({ original_url: url, short_url: count });
         }
     });
 };
 
 exports.getUrl = (req, res) => {
     const shortUrl = req.params.short_url;
-    // readFile
     fs.readFile(`${__dirname}/../data/${shortUrl}`, 'utf8', (err, data) => {
         if (err) {
             res.json({ error: `No short URL found for the given input` });
@@ -24,14 +24,10 @@ exports.getUrl = (req, res) => {
 };
 
 exports.postUrl = (req, res) => {
-    // get the url from the body
     const url = req.body.url;
-    // check if the url is valid
     if (validUrl.isUri(url)) {
-        // writeFile
         writeFile(url, req, res);
     } else {
-        // if not valid, response with an error
         res.json({ error: 'invalid url' });
     }
 };
