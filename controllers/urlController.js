ed06389891482bc956a1618c2431fa5f6d98bd1c
Rafał Kowalczyk <kowalczykr@trend-glass.local>
2022-05-11 15:04:33 +0200

    first test

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,9 +1,37 @@
+const fs = require('fs');
+const validUrl = require('valid-url');
+
+const writeFile = async (url, req, res) => {
+    await fs.writeFile(`${__dirname}/../data/1`, url, (err) => {
+        if (err) {
+            res.json({ error: err });
+        } else {
+            res.json({ original_url: url, short_url: 1 });
+        }
+    });
+};
+
 exports.getUrl = (req, res) => {
-    //{ error: 'invalid url' }
-    res.json({ date: req.params.short_url });
+    const shortUrl = req.params.short_url;
+    // readFile
+    fs.readFile(`${__dirname}/../data/${shortUrl}`, 'utf8', (err, data) => {
+        if (err) {
+            res.json({ error: `No short URL found for the given input` });
+        } else {
+            res.redirect(data);
+        }
+    });
 };
 
 exports.postUrl = (req, res) => {
-    //{ original_url : 'https://freeCodeCamp.org', short_url : 1}
-    res.json({ date: req.body.url });
+    // get the url from the body
+    const url = req.body.url;
+    // check if the url is valid
+    if (validUrl.isUri(url)) {
+        // writeFile
+        writeFile(url, req, res);
+    } else {
+        // if not valid, response with an error
+        res.json({ error: 'invalid url' });
+    }
 };
