0547da392eb83a132d233d4bca4cafdeffec2464
Rafał Kowalczyk <raff1410X@gmail.com>
2022-05-11 19:49:35 +0200

    Heroku final commit

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,32 +1,43 @@
 const fs = require('fs');
 const validator = require('validator');
 
-const writeFile = async (url, req, res) => {
-    const count = fs.readdirSync(`${__dirname}/../data`).length + 1;
-    fs.writeFile(`${__dirname}/../data/${count}`, url, (err) => {
-        if (err) {
-            res.json({ error: err });
-        } else {
-            res.json({ original_url: url, short_url: count });
-        }
-    });
-};
-
 exports.getUrl = (req, res) => {
     const shortUrl = req.params.short_url;
-    fs.readFile(`${__dirname}/../data/${shortUrl}`, 'utf8', (err, data) => {
+    fs.readFile(`${__dirname}/../data/${shortUrl}`, 'utf8', (err, url) => {
         if (err) {
-            res.json({ error: `No short URL found for the given input` });
+            res.json({ error: 'No short URL found for the given input' });
         } else {
-            res.redirect(data);
+            res.redirect(url);
         }
     });
 };
 
-exports.postUrl = (req, res) => {
+const countFiles = () =>
+    new Promise((resolve, reject) => {
+        fs.readdir(`${__dirname}/../data/`, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(files.length + 1);
+            }
+        });
+    });
+
+exports.postUrl = async (req, res) => {
     const url = req.body.url;
     if (validator.isURL(url)) {
-        writeFile(url, req, res);
+        try {
+            const count = await countFiles();
+            fs.writeFile(`${__dirname}/../data/${count}`, url, (err) => {
+                if (err) {
+                    res.json({ error: err });
+                } else {
+                    res.json({ original_url: url, short_url: count });
+                }
+            });
+        } catch (err) {
+            res.json({ error: 'Error counting files' });
+        }
     } else {
         res.json({ error: 'invalid url' });
     }
